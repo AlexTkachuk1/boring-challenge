dbacf8ec61716643c7f213e7dbcf2a296490dba4
AlexTkachuk1 <Lyucifer97@list.ru>
2024-12-30 16:36:01 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import Phaser from "phaser";
 import "phaser/plugins/spine/dist/SpinePlugin";
 import SliderPlugin from "phaser3-rex-plugins/plugins/slider-plugin.js";
 
-import { Boot } from "./scenes/Boot";
-import { Game } from "./scenes/Game";
-import { Preloader } from "./scenes/Preloader";
+import { Boot } from "./Scees/Boot";
+import { Game } from "./Scenes/Game";
+import { Preloader } from "./Scenes/Preloader";
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
